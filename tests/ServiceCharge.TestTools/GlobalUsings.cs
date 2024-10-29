b03c1350c63abaafb164b00c0a75bd7c66be6cca
Y0l0B0T <@Y0l0B0T>
2024-10-29 13:08:08 +0330

    Update Project

---
+++ tests/ServiceCharge.TestTools/GlobalUsings.cs
@@ -0,0 +1,5 @@
+﻿global using ServiceCharge.Entities.Blocks;
+global using ServiceCharge.Entities.Floors;
+global using ServiceCharge.Services.Blocks.Contracts.Dtos;
+global using ServiceCharge.Services.Floors.Contracts.Dtos;
+global using ServiceCharge.Services.Units.Contracts.Dtos;
